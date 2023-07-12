37ee52ef0a5941495d19d8527b7ef6038273eb4f
Mojmir Novakovic <mojmir.novakovic@gmail.com>
2023-07-12 08:58:35 +0200

    first working version

--- js/kolo_content.js
+++ js/kolo_content.js
@@ -1,48 +1,36 @@
-$(function() {
-	chrome.runtime.onMessage.addListener(
-		function(request, sender, sendResponse) {
-		if (request.url == location.href)
-			this.addKolo();
-		  	sendResponse({farewell: "goodbye"});
-		});
 
-});
-function addKolo(){
-	var body = $('body');
-	if (body.parents('iframe').length==0){
-		body.append('<canvas id="koloCanvas" width="100" height="100" style="z-index:1000;position:fixed; top:10px; right:10px;"></canvas>')
-	}
-	var canvas = document.getElementById('koloCanvas');
-	var context = canvas.getContext('2d');
-	var centerX = canvas.width / 2;
-	var centerY = canvas.height / 2;
-	var radius = 10;
+function getMaxImage(data) {
+  var maxDimension = 0;
+  var maxImage = null;
 
-	context.beginPath();
-	context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-	context.fillStyle = 'white';
-	
-	context.fill();
-	context.shadowColor = '#999';
-	context.lineWidth = 6;
-	context.strokeStyle = 'red';
-	context.shadowColor = "transparent";
-	context.stroke();
-	context.beginPath();
-	context.arc(centerX, centerY, radius+12, 0, 2 * Math.PI, false);
-	context.fillStyle = 'transparent';
-	context.fill();
-	context.lineWidth = 8;
-	context.strokeStyle = 'black';
-	context.shadowColor = '#999';
-	context.shadowBlur = 6;
-	context.shadowOffsetX = -3;
-	context.shadowOffsetY = 3;
-	context.stroke();
-	
+  if (window.location !== window.parent.location)
+    return null;
+
+  var imgElements = document.images;
+  for (var index in imgElements) {
+    var img = imgElements[index];
+    var currDimension = img.width * img.height;
+    if (currDimension  > maxDimension){
+       maxDimension = currDimension
+       maxImage = img;
+    }
+  }
+  if (maxImage)
+    addMapToImage(maxImage, data)
 }
 
+function addMapToImage(image, data){
+  image.setAttribute('data-kolo-link', data.localStorage.link[0].id);
+  image.setAttribute('data-kolo-zoom', 16 );
+  image.setAttribute('name', 'kolo-location');
+  initKolo();
+}
 
-
-
-
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.method === 'getMaxImage') {
+    var maxImage = getMaxImage(request);
+    sendResponse({ data: maxImage });
+  }else{
+    sendResponse({});
+  }
+});
