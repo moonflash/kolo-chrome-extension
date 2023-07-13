3eeb58c4b17185bb16b3d7005b6a03e932f7242e
Mojmir Novakovic <mojmir.novakovic@gmail.com>
2023-07-13 20:40:56 +0200

    update image on popup action

--- js/popup.js
+++ js/popup.js
@@ -3,11 +3,12 @@ const map = document.getElementById('map');
 const enabled = document.getElementById('enabled');
 const image_enabled = document.getElementById('image_enabled');
 const linkel = document.getElementById('links');
-
+var links;
 chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
   chrome.tabs.sendMessage(tabs[0].id, {method:'getLinks'}, function (data){
     var li = document.createElement("li");
     var a = document.createElement('a');
+    links = data.links
     if(data.links.length == 1){
       a.href = "https://kolo.it/link/" + data.links[0].id;
       a.textContent = "Single link in Kolo: " + data.links[0].kolo_id ;
@@ -25,10 +26,12 @@ chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
 });
 zoom.addEventListener('change', function() {
   chrome.storage.local.set({zoom: zoom.value});
+  updateImage()
 });
 
 map.addEventListener('change', function() {
   chrome.storage.local.set({map: map.value});
+  updateImage()
 });
 
 enabled.addEventListener('change', function() {
@@ -52,3 +55,8 @@ chrome.storage.local.get("enabled", function (retrieved_data){
 chrome.storage.local.get("image_enabled", function (retrieved_data){
   image_enabled.checked = retrieved_data.image_enabled;
 });
+function updateImage() {
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { method: 'getMaxImage', links: links}, function(response) {})
+  });
+}
